6698ada693cbb9ed57b2eab6d7279d167fccdef5
Mark <mark.a.mcadam@gmail.com>
2020-07-07 11:57:30 -0700

    changes structure

---
+++ guess_the_number.py
@@ -0,0 +1,25 @@
+"""
+Assumptions
+generate random number unknown to the user 
+
+
+
+"""
+
+# understand
+
+# plan
+
+# import random module
+
+# game has a while loop
+    # start the game with message to the user what to expect
+    # get user input about game settings. 
+    # initialize random number at the start 
+
+
+
+# execute 
+
+# reflect
+
