d6b01f2e257c9109c0fdb63d9173ddb4221d43df
Mark <mark.a.mcadam@gmail.com>
2020-09-10 08:09:47 -0700

    minor tweaks

--- graphs/projects/ancestor/ancestor.py
+++ graphs/projects/ancestor/ancestor.py
@@ -61,10 +61,3 @@ def earliest_ancestor(ancestors, starting_node):
                 if to_visit.size() == 0:
                     return final_v
 
-
-test = earliest_ancestor(
-    [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)],
-    3,
-)
-
-print(test)
